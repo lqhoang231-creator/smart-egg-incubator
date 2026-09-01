691677e83fada3708f36ea9d443ac804543b8eaf
lqhoang231-creator <lqhoang231@gmail.com>
2026-09-01 13:07:27 +0700

    dc control code

--- firmware/Driving/Driving.ino
+++ firmware/Driving/Driving.ino
@@ -1,3 +1,7 @@
+#define Pin_ain1 17
+#define Pin_ain2 5
+#define Pin_pwma 15
+#define Pin_stby 16
 const int servoPin = 18;
 const int channel = 0;
 unsigned long preMillis = 0;
@@ -5,22 +9,59 @@ const long interval1 = 2000;
 const long interval2 = 5000;
 int n = 0;
 const int buttonPin = 4;
+const int channel_motor = 4;
+
+int mode = 1;
+int lastButtonState = HIGH;
+int buttonState = HIGH;
+unsigned long lastDebounceTime = 0;
+unsigned long delayTime = 50;
+
+unsigned long preMillisMotor = 0;
+int run = 5000;
+int stop = 5000;
+bool mtRunning = true;
 
 void setup() {
     ledcSetup(channel, 50, 16);     //channel 0, frequency 50 Hz, resolution 16 bit
     ledcAttachPin(servoPin, channel);
     pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(Pin_ain1, OUTPUT);
+    pinMode(Pin_ain2, OUTPUT);
+    pinMode(Pin_pwma, OUTPUT);
+    pinMode(Pin_stby, OUTPUT);
+    digitalWrite(Pin_stby, HIGH);
+    ledcSetup(channel_motor, 1000, 8);
+    ledcAttachPin(Pin_pwma, channel_motor);  
+    motor(150);   
 }
 
 void loop() {
-    //unsigned long currentMillis = millis();
-    if (digitalRead(buttonPin) == HIGH){
-        delay(50);
-        if (digitalRead(buttonPin) == HIGH)
-            servo1();
+    unsigned long Millis = millis();
+    int read = digitalRead(buttonPin);
+    if(read != lastButtonState){
+        lastDebounceTime = Millis;
     }
+    if(Millis - lastDebounceTime >= delayTime){
+        if(read != buttonState){
+            buttonState = read;
+            if(buttonState == LOW){
+                mode = (mode == 1) ? 2:1;
+            }
+        }
+    }
+    lastButtonState = read;
+    if(mode == 1)
+        servo1();
     else
         servo2();
+
+    unsigned long interval = (mtRunning) ? run:stop;
+    if(Millis - preMillisMotor >= interval){
+    preMillisMotor = Millis;
+    mtRunning = !mtRunning;
+    motor(mtRunning ?150:0);
+  }
 }
 
 void servo1 (){
@@ -51,4 +92,11 @@ void servo2(){
             n = 0;
         }
     }
+}
+
+void motor(int speed){
+    speed = constrain(speed, 0, 255);
+    digitalWrite(Pin_ain1, HIGH);
+    digitalWrite(Pin_ain2, LOW);
+    ledcWrite(channel_motor, speed);
 }
